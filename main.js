719d9bd6452a920eef04107a4bde952390fb88d0
vladislav.panov <vladislav.panov@office.e2e4gu.ru>
2023-07-27 19:34:00 +0700

    added feels like

--- main.js
+++ main.js
@@ -1,78 +1,68 @@
-const apiKey = "467b9468ac57b443044062033ccbfea0";
+document.addEventListener("DOMContentLoaded", function () {
+  const apiKey = "467b9468ac57b443044062033ccbfea0";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const searchInput = document.querySelector(".search-box input");
+  const searchButton = document.querySelector(".fa-magnifying-glass");
+  const navigationButton = document.querySelector(".fa-location-dot")
+  const images = document.querySelector(".images");
+  let loc;
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
-
-const searchInput = document.querySelector(".search-box input");
-
-const searchButton = document.querySelector(".search-box button");
-
-const weatherIcon = document.querySelector(".weather-image i");
-
-const weather = document.querySelector(".weather");
-
-const images = document.querySelector(".images");
-
-
-async function checkWeather(city) {
- 
-  const response = await fetch(apiUrl + city + '&appid=' + apiKey);
-  
-  data = await response.json();
-  
-  if (data.message === 'Nothing to geocode') {
-    alert('Введите город!');
- 
-  } else if (data.cod == 404) {
-    alert('К сожалению я не знаю такого города :(')
-  }
-  else  {
+  async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + '&appid=' + apiKey + "&lang=ru");
+    const data = await response.json();
+    
+    console.log(Math.round(data.main.feels_like - 273.1) )
+    if (data.message === "Nothing to geocode") {
+      alert("Введите город!");
+    } else if (data.cod == 404) {
+      alert("К сожалению я не знаю такого города :(");
+    } else {
+    document.querySelector(".feels_like").innerHTML = `Ощущается как ${Math.round(data.main.feels_like - 273.1)} &#8451`;
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML =
-      Math.round(data.main.temp) + "&#8451";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp - 273.1) + "&#8451";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-    if (data.weather[0].main == "Clear") {
-      document.querySelector(".info").innerHTML = data.weather[0].description;
-      images.src = './images/Clear.png'
-
-    } else if (data.weather[0].main == "Rain") {
-      document.querySelector(".info").innerHTML = data.weather[0].description;
-      images.src = './images/Rain.png'
-   
-    } else if (data.weather[0].main == "Mist") {
-      document.querySelector(".info").innerHTML = data.weather[0].description;
-      images.src = './images/Mist.png'
-    
-    } else if (data.weather[0].main == "Drizzle") {
-      document.querySelector(".info").innerHTML = data.weather[0].description;
-      images.src = './images/Drizzle.png'
-    
-    } else if (data.weather[0].main == 'Clouds') {
-      images.src = './images/Clouds.png'
-      document.querySelector(".info").innerHTML = data.weather[0].description;
+    document.querySelector(".wind").innerHTML = data.wind.speed + " м/с";
+      if (data.weather[0].main == "Clear") {
+        document.querySelector(".info").innerHTML = data.weather[0].description;
+        images.src = './images/Clear.png'
+      } else if (data.weather[0].main == "Rain") {
+        document.querySelector(".info").innerHTML = data.weather[0].description;
+        images.src = './images/Rain.png'
+      } else if (data.weather[0].main == "Mist") {
+        document.querySelector(".info").innerHTML = data.weather[0].description;
+        images.src = './images/Mist.png'
+      } else if (data.weather[0].main == "Drizzle") {
+        document.querySelector(".info").innerHTML = data.weather[0].description;
+        images.src = './images/Drizzle.png'
+      } else if (data.weather[0].main == 'Clouds') {
+        images.src = './images/Clouds.png'
+        document.querySelector(".info").innerHTML = data.weather[0].description;
+      }
     }
   }
-}
 
-
-ymaps.ready(function(){
-  ymaps.geolocation.get({
-      provider: 'yandex'
-  }).then(function (result) {
-      let loc = result.geoObjects.get(0).properties._data.metaDataProperty.GeocoderMetaData.Address.formatted;
-      checkWeather(loc)
+  ymaps.ready(function () {
+    ymaps.geolocation.get({
+      provider: "yandex",
+    }).then(function (result) {
+      loc = result.geoObjects.get(0).properties._data.metaDataProperty.GeocoderMetaData.Address.formatted;
+      checkWeather(loc);
+    });
   });
-});
 
-searchButton.addEventListener("click", () => {
-  checkWeather(searchInput.value);
-  searchInput.value = "";
-});
+  navigationButton.addEventListener("click", () => {
+    checkWeather(loc)
+  })
 
-searchInput.addEventListener("keydown", (event) => {
-  if (event.keyCode === 13) {
+  searchButton.addEventListener("click", () => {
     checkWeather(searchInput.value);
     searchInput.value = "";
-  }
+  });
+
+  searchInput.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+      checkWeather(searchInput.value);
+      searchInput.value = "";
+    }
+  });
 });
